d1f1abc420582a91cd7f38bec6da233f3844765a
simpleton <chazesingh@gmail.com>
2024-05-08 14:57:41 +0530

    ADD: controller mapping for CRUD without service

--- src/main/java/com/job/controller/JobController.java
+++ src/main/java/com/job/controller/JobController.java
@@ -1,24 +1,38 @@
 package com.job.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.job.entities.Job;
 import com.job.repository.JobRepository;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api-v1")
 public class JobController {
-		@Autowired
-		JobRepository jobRepository;
+
+
+		//mapping to create a new user
 		@PostMapping("/jobs")
-		
 		public String createNewJob(@RequestBody Job job) {
-			jobRepository.save(job);
-			return "Job Created";
-			
 		}
+		//mapping to fetch all the users
+		@GetMapping("/jobs")
+		public List<Job> getAllJobs(){
+
+		}
+		//mapping to fetch a single user
+		@GetMapping("/jobs/{jobId}")
+		public Job getJobById(@PathVariable Long jobId){
+
+		}
+
+		//mapping to delete a user
+		@DeleteMapping("/jobs/{jobId}")
+		public String delJobById(@PathVariable Long jobId){
+			String ans="DONE";
+			return ans;
+		}
+
 }
