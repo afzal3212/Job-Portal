02b70ea21ea6ae849d584a198cbbad348f6137a8
simpleton <chazesingh@gmail.com>
2024-05-08 15:32:54 +0530

    implmented the service using repo

--- src/main/java/com/job/controller/JobController.java
+++ src/main/java/com/job/controller/JobController.java
@@ -2,35 +2,48 @@ package com.job.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.job.service.jobServiceImpl;
 
 import com.job.entities.Job;
 import com.job.repository.JobRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api-v1")
 public class JobController {
 
+		@Autowired
+		private jobServiceImpl JSI;
+
+		@Autowired
+		public JobController(jobServiceImpl JSI){
+			this.JSI=JSI;
+		}
 
 		//mapping to create a new user
 		@PostMapping("/jobs")
-		public String createNewJob(@RequestBody Job job) {
+		public Job createNewJob(@RequestBody Job job) {
+			Job newJob=JSI.makeJob(job);
+			return newJob;
 		}
 		//mapping to fetch all the users
 		@GetMapping("/jobs")
 		public List<Job> getAllJobs(){
-
+			return JSI.getAllJobs();
 		}
 		//mapping to fetch a single user
 		@GetMapping("/jobs/{jobId}")
-		public Job getJobById(@PathVariable Long jobId){
-
+		public Optional<Job> getJobById(@PathVariable Long jobId){
+			Optional<Job> newJob=JSI.getJobFromId(jobId);
+			return newJob;
 		}
 
 		//mapping to delete a user
 		@DeleteMapping("/jobs/{jobId}")
 		public String delJobById(@PathVariable Long jobId){
+			JSI.remJobFromId(jobId);
 			String ans="DONE";
 			return ans;
 		}
--- src/main/java/com/job/repository/JobRepository.java
+++ src/main/java/com/job/repository/JobRepository.java
@@ -3,7 +3,9 @@ package com.job.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.job.entities.Job;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobRepository extends JpaRepository<Job,Long> {
 	
 }
